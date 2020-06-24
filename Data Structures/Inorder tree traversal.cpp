73dd674d3704d470ae67b80e54990859e4bf41fb
Gaurav Sachdev <gaurav1565@gmail.com>
2020-06-24 15:03:20 +0530

    inorder tree traversal

---
+++ Data Structures/Inorder tree traversal.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void inOrder(Node *root) 
+    {
+        if(root->left != NULL)
+        {
+            inOrder(root->left);
+        }
+        if(root->data != NULL)
+        {
+            cout<<root->data<<" ";
+        }
+        if(root->right != NULL)
+        {
+            inOrder(root->right);
+        }
+    }
+
+}; //End of Solution
